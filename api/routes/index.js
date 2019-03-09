94a7a2ef851981211d2c875fcd947875909d2993
rbtkay <robert.g.khayat@gmail.com>
2019-03-09 03:44:17 +0200

    product api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userRoutes = require('./user');
+const productRoutes = require('./product');
 
 router.use('/user', userRoutes);
+router.use('/product', productRoutes);
 
 module.exports = router;
--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,8 @@
 const User = require('../objects/user');
 const router = require('express').Router();
 
-router.get('/All', User.getUser); 
-router.get('/ByEmail', User.getUserByEmail);
+router.get('/all', User.getAllUser)
+router.get('/byEmail', User.getUserByEmail);
 router.get('/auth', User.authUser);
 
 module.exports = router;
