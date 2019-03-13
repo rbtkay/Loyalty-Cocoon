e266d1e686b5aaa535584648d88d0b62557a2465
KVNbog <kevin.boghossian@gmail.com>
2019-03-13 19:57:44 +0200

    fix session

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = express();
 const next = require('next');
+const session = require('express-session');
 
 const app = next({
     dev: process.env.NODE_ENV !== 'production'
@@ -11,13 +12,14 @@ const handle = app.getRequestHandler(app);
 app.prepare().then(() => {
 
     const apiRoutes = require('./api/routes/index');
+    const verify = require('./verify');
 
+    server.use(session({secret: 'un truc bizarre', saveUninitialized: true, resave: true}));
     server.use("/api", apiRoutes);
-    
     server.get("*", (req, res)=>{
         return handle(req,res);
     });
-    
+
     const port = 8000;
 
     server.listen(port, () => {
