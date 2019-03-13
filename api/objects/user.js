c0e331eb4584dedb3c8436019bf7551d08ea719e
KVNbog <kevin.boghossian@gmail.com>
2019-03-13 19:13:35 +0200

    fixing signin

--- api/objects/user.js
+++ api/objects/user.js
@@ -21,16 +21,21 @@ exports.getUserByEmail = (req, res, next) => {
 }
 
 exports.authUser = (req, res, next) => {
-    var email = req.query.email;
+    var username = req.query.username;
     var password = req.query.password;
 
-    if (email && password) {
-        mysqlConnection.query('select * from User_T where user_email = ? and user_password = ?', [email, password], (err, result, fields) => {
+    if (username && password) {
+        mysqlConnection.query('select * from User_T where user_username = ? and user_password = ?', [username, password], (err, result, fields) => {
             if (err) throw err;
             if(result.length > 0){
+                req.session.username = username;
+                console.log(req.session.username);
                 res.send(result);
-            }else{
-                res.redirect("/");
+            } else {
+                const errorObj = {
+                    "message": "Invalid Username/Password"
+                }
+                res.send(errorObj);
             }
         });
     }
