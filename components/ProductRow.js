52857e2b6a270913893bdc8a721fc8139f6cbda0
KVNbog <kevin.boghossian@gmail.com>
2019-03-17 21:57:45 +0200

    feat: first colors (navbar)

--- components/ProductRow.js
+++ components/ProductRow.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import ProductCard from './ProductCard';
-import { Card } from 'semantic-ui-react';
+import { Card, CardGroup } from 'semantic-ui-react';
+import { Router } from '../routes';
 
 class ProductRow extends Component {
+        state = {
+            products: [],
+            filter: "all"
+        }
 
     render() {
-        const { products } = this.props;
-        if (products) {
+
+        console.log(this.props.filter);
+        if (this.state.products.length > 0) {
             return (
-                <Card.Group>
-                    {this.renderProducts(products)}
-                </Card.Group>
+                <CardGroup>
+                    {this.renderProducts(this.state.products)}
+                </CardGroup>
             );
-        } else {
+        }
+        else {
             return <div>Loading Products...</div>
         }
     }
 
     renderProducts(products) {
-        if (products) {
+        if (this.state.products) {
             return products.map((object) => {
                 return (
                     <ProductCard
@@ -30,6 +37,37 @@ class ProductRow extends Component {
                 );
             })
         }
+    }
+
+    async componentDidMount() {
+        const filter = this.state.filter;
+        console.log("filter in component did mount" + filter);
+
+        const response = await fetch(`http://localhost:8000/api/product/${filter}`, {
+            headers: new Headers({
+                'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXN1bHQiOlt7InVzZXJfdXNlcm5hbWUiOiJyYnRrYXkiLCJ1c2VyX2VtYWlsIjoicm9iZXJ0QGdtYWlsLmNvbSIsInVzZXJfcGFzc3dvcmQiOiJoYWhhaGEiLCJ1c2VyX25hbWUiOiJyb2JlcnQiLCJ1c2VyX2RvYiI6IjIwMTktMDMtMDFUMjI6MDA6MDAuMDAwWiIsInVzZXJfZ2VuZGVyIjoiTSIsInVzZXJfcGhvbmUiOiI3MDY1NzMwMCIsInVzZXJfcHJlZnMiOiJuZXNjYWZlIiwidXNlcl9hZGRyZXNzIjoiMHhhZXJic2VyYnNiIiwidXNlcl9jb3VudHJ5IjoiTGViYW5vbiIsInVzZXJfcHJvZmVzc2lvbiI6IkRvY3RvciIsInVzZXJfb3JnYW5pemF0aW9uIjoiSG90ZWwgRGlldSJ9XSwiaWF0IjoxNTUyNjQxNDA3fQ.Ao-ZJ9yifCnnjInOWC6gRwleSYCHmJ9Ob1L6-jQsKF0"
+            })
+        });
+        const products = await response.json();
+        this.setState({ products });
+    }
+
+    async componentWillReceiveProps() {
+
+        const { filter } = this.props;
+        console.log("filter in componentWillReceiveProps" + filter);
+
+
+        const response = await fetch(`http://localhost:8000/api/product/${filter}`, {
+            headers: new Headers({
+                'authorization': "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZXN1bHQiOlt7InVzZXJfdXNlcm5hbWUiOiJyYnRrYXkiLCJ1c2VyX2VtYWlsIjoicm9iZXJ0QGdtYWlsLmNvbSIsInVzZXJfcGFzc3dvcmQiOiJoYWhhaGEiLCJ1c2VyX25hbWUiOiJyb2JlcnQiLCJ1c2VyX2RvYiI6IjIwMTktMDMtMDFUMjI6MDA6MDAuMDAwWiIsInVzZXJfZ2VuZGVyIjoiTSIsInVzZXJfcGhvbmUiOiI3MDY1NzMwMCIsInVzZXJfcHJlZnMiOiJuZXNjYWZlIiwidXNlcl9hZGRyZXNzIjoiMHhhZXJic2VyYnNiIiwidXNlcl9jb3VudHJ5IjoiTGViYW5vbiIsInVzZXJfcHJvZmVzc2lvbiI6IkRvY3RvciIsInVzZXJfb3JnYW5pemF0aW9uIjoiSG90ZWwgRGlldSJ9XSwiaWF0IjoxNTUyNjQxNDA3fQ.Ao-ZJ9yifCnnjInOWC6gRwleSYCHmJ9Ob1L6-jQsKF0"
+            })
+        });
+        const products = await response.json();
+
+        console.log("working" + products);
+        this.setState({ products });
+        console.log(this.state.products);
     }
 }
 
