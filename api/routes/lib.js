ba183dfe435e82a1c437cf1b4d0f0f7ba8c2f1c1
rbtkay <robert.g.khayat@gmail.com>
2019-04-07 23:12:53 +0300

    feat: api that send verification code to user done.

--- api/routes/lib.js
+++ api/routes/lib.js
@@ -4,5 +4,6 @@ const router = require('express').Router();
 router.get('/confirmEmail', Lib.sendConfirmEmail);
 router.get('/verify', Lib.verifyEmail);
 router.get('/usernamesEmails', Lib.getUsernamesEmails);
+router.get('/code', Lib.sendCode);
 
 module.exports = router;
