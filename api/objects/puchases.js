eb4317ea536222b6ddb811fdde417f54a6c9a147
rbtkay <robert.g.khayat@gmail.com>
2019-04-17 22:05:28 +0300

    feat: refactor purchase Api

--- api/objects/puchases.js
+++ api/objects/puchases.js
@@ -1,10 +1,9 @@
 const mysqlconnection = require('../../database/connection');
 
-//FIXME: Refactor
 exports.getPurchasesByVendor = (req, res) => {
     const vendor = req.query.username;
 
-    mysqlconnection.query('select purchase_t.*, product_t.product_name from purchase_t, product_t where purchase_t.vendor_id = ? and purchase_t.product_id = product_t.product_id', [vendor], (err, result) => {
+    mysqlconnection.query('select purchase_t.*, product_t.product_name, user_t.user_username from purchase_t, product_t, user_t where purchase_t.vendor_id = (select user_id from user_t where user_username = ?) and purchase_t.product_id = product_t.product_id and purchase_t.vendor_id = user_t.user_id', [vendor], (err, result) => {
         if (err) throw err;
         if (result.length > 0) {
             res.status(200).send(result);
@@ -14,14 +13,14 @@ exports.getPurchasesByVendor = (req, res) => {
     });
 }
 
-//FIXME: Refactor
 exports.getPurchasesByUser = (req, res) => {
     const username = req.query.username;
 
-    mysqlconnection.query('select purchase_t.*, product_t.product_name from purchase_t, product_t where purchase_t.cust_id = ? and purchase_t.product_id = product_t.product_id', [username], (err, result) => {
-        if (err) throw err;
-        res.status(200).send(result);
-    })
+    mysqlconnection.query('select purchase_t.*, product_t.product_name, user_t.user_username from purchase_t, product_t, user_t where purchase_t.cust_id = (select user_id from user_t where user_username = ?) and purchase_t.product_id = product_t.product_id and purchase_t.vendor_id = user_t.user_id',
+        [username], (err, result) => {
+            if (err) throw err;
+            res.status(200).send(result);
+        })
 }
 
 exports.finalizePurchase = (req, res) => {
@@ -33,12 +32,11 @@ exports.finalizePurchase = (req, res) => {
     });
 }
 
-//FIXME: Refactor
 exports.getPurchaseByVendorUser = (req, res) => {
     const vendorUsername = req.query.vendorUsername;
     const customerUsername = req.query.userUsername;
 
-    mysqlconnection.query('select purchase_t.*, product_t.product_name from purchase_t, product_t where (purchase_t.vendor_id = ? and purchase_t.cust_id = ?) and purchase_t.product_id = product_t.product_id', [vendorUsername, customerUsername], (err, result) => {
+    mysqlconnection.query('select purchase_t.*, product_t.product_name, user_t.user_username from purchase_t, product_t, user_t where (purchase_t.vendor_id = (select user_id from user_t where user_username = ?) and purchase_t.cust_id = (select user_id from user_t where user_username = ?)) and purchase_t.product_id = product_t.product_id and purchase_t.vendor_id = user_t.user_id', [vendorUsername, customerUsername], (err, result) => {
         if (err) throw err;
         if (result.length > 0) {
             res.status(200).send(result);
@@ -48,11 +46,10 @@ exports.getPurchaseByVendorUser = (req, res) => {
     });
 }
 
-//FIXME: Refactor
 exports.addPurchase = (req, res) => {
     const { username, productId, vendorUsername, purchaseTime } = req.query;
 
-    mysqlconnection.query('insert into purchase_t (cust_id, product_id, vendor_id, purchase_date) values (?,?,?,?)', [username, productId, vendorUsername, purchaseTime], (err, result) => {
+    mysqlconnection.query('insert into purchase_t (cust_id, product_id, vendor_id, purchase_date) values ((select user_id from user_t where user_t.user_username = ?),2,(select user_id from user_t where user_t.user_username = ?),?)', [username, productId, vendorUsername, purchaseTime], (err, result) => {
         if (err) throw err;
         res.status(200).send(result);
     });
